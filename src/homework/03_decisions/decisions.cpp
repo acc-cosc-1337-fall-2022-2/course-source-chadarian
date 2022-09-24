b2754828b3c70107e408c8582b8414901f12a98e
chadarian <josh.ely@g.austincc.edu>
2022-09-24 23:58:45 +0000

    Homework 03 complete

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,55 @@
 //write include statement for decisions header
+#include "decisions.h"
+#include <iostream>
+#include<string>
+
+using std::cout; 
+using std::cin;
+using std::string;
 
 
 //Write code for function(s) code here
+
+string get_letter_grade_using_if(int grade)
+{
+    if(grade >= 90)
+    {
+        return "A";
+    }
+    else if(grade >= 80)
+    {
+        return "B";
+    }
+    else if(grade >= 70)
+    {
+        return "C";
+    }
+    else if(grade >= 60)
+    {
+        return "D";
+    }
+    else
+    {
+        return "F";
+    }
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    std::string letter;
+    switch(grade)
+    {
+        case 90 ... 100: letter = "A"; break;
+
+        case 80 ... 89: letter = "B"; break;
+
+        case 70 ... 79: letter = "C"; break;
+
+        case 60 ... 69: letter = "D"; break;
+
+        default : letter = "F";
+    }
+
+    return letter;
+
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,38 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+#include<string>
+
+using std::cout; 
+using std::cin;
+using std::string;
+
 
 int main() 
 {
+
+	// var declaration
+
+	int grade_if;
+	int grade_switch;
+	string sgrade_if;
+	string sgrade_switch;
+
+	cout << "Please enter a grade between 0-100 (If statement): ";
+	cin >> grade_if;
+	cout << "Please enter a grade between 0-100 (Switch statement): ";
+	cin >> grade_if;
+
+//=====================================================================
+//Function calls
+//=====================================================================
+
+sgrade_if = get_letter_grade_using_if(grade_if);
+sgrade_switch = get_letter_grade_using_switch(grade_switch);
+
+cout << "Your If grade is a " << sgrade_if << "\n";
+
+cout << "Your Switch grade is a " << sgrade_switch << "\n";
+
 	return 0;
 }
