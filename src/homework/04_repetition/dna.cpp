97b071e0b36571b9c7d7aa7a3dce105ee637b5ee
chadarian <josh.ely@g.austincc.edu>
2022-10-02 01:29:46 +0000

    HW04 complete

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,37 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+
+int factorial(int num)
+{
+
+    int sum = 1;
+    for(int i = 1; i <= num; ++i) 
+    {
+        sum *= i;
+    }
+    return sum;
+
+}
+
+int gcd(int num1, int num2)
+{
+
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        else
+        {
+            num1 = num1 - num2;
+        }
+    }
+
+return num1;
+
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,5 +1,7 @@
 //write include statements
-
+#include <iostream>
+#include "dna.h"
+using namespace std;
 //write using statements
 
 
@@ -12,5 +14,66 @@ user enters a y or Y.
 */
 int main() 
 {
+
+	int reply = 0;
+	int exit = 1;
+
+	do
+	{
+
+		cout << "\nFor Factorials enter 1 \n";
+		cout << "For GCD enter 2 \n";
+		cout << "To Exit enter 3 \n\n";
+		cin >> reply;
+
+		if (reply == 1)
+		{
+			int num1;
+			cout << "Please enter a number for your factorial :\n";
+			cin >> num1;
+			cout << "Your annswer is :" << factorial(num1) << endl;
+
+		}
+		
+		if (reply == 2)
+		{
+			int num1;
+			int num2;
+			cout << "Please enter two numbers for your GCD :\n";
+			cin >> num1;
+			cin >> num2;
+			cout << "Your annswer is :" << gcd(num1, num2) << endl;
+		}
+
+		if (reply == 3)
+		{
+
+			string exit_reply;
+
+			cout << "Are you sure you want to exit? Please type yes or no in lowercase\n";
+			cin >> exit_reply;
+			if (exit_reply == "yes")
+			{
+				cout << "Goodbye\n";
+				exit = 0;
+			}
+			
+			else if (exit_reply == "no")
+			{
+				cout << " Back to the menu\n";
+			}
+
+			else
+			{
+				cout <<"=================================\n";
+				cout << "Make sure you use lower case\n";
+				cout <<"=================================\n";
+			}
+		}
+
+	}
+	while (exit == 1);
+
+
 	return 0;
 }
