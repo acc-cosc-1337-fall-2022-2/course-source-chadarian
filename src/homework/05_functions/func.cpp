7036d7d27efefd2f8f3d34fc8dd77185641a42b6
chadarian <josh.ely@g.austincc.edu>
2022-10-09 02:28:14 +0000

    HW05 complete

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,45 @@
 //add include statements
-
+#include "func.h"
+#include <string>
+#include <iostream>
 //add function code here
+
+double get_gc_content(std::string dna)
+{
+    double total = dna.length();
+    double num = 0;
+
+    for(char c : dna)
+    {
+        if( c == 'G'|| c == 'g') num++;
+        else if( c == 'C'|| c == 'c') num++;
+    }
+
+    return (num / total);
+}
+
+std::string get_reverse_string(std::string dna)
+{
+    int num = dna.length();
+
+    for (int i = 0; i < num / 2; i++)
+        std::swap(dna[i], dna[num - i - 1]);
+   
+   return dna;
+}
+
+std::string get_dna_complement(std::string dna)
+{
+    size_t size = dna.length();
+    size_t num = 0;
+
+    for( char &c : dna)
+    {
+        if( c == 'G' || c == 'g' ) c = 'C';
+        else if (c == 'C' || c == 'c') c = 'G';
+        else if (c == 'A' || c == 'a') c = 'T';
+        else if (c == 'T' || c == 't') c = 'A';
+    }
+
+    return get_reverse_string(dna);
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,65 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
-
-*/
 int main() 
 {
+	int reply = 0;
+	int exit = 1;
 
+	do
+	{
+		cout << "\nFor GC Content enter 1 \n";
+		cout << "For DNA Complement enter 2 \n";
+		cout << "To Exit enter 3 \n\n";
+		cin >> reply;
+
+		if (reply == 1)
+		{
+			string dna;
+			cout << "Please enter a DNA strand to check GC content :\n";
+			cin >> dna;
+			cout << "The GC conent is :" << get_gc_content(dna) << endl;
+
+		}
+		
+		if (reply == 2)
+		{
+			string dna;
+			cout << "Please enter a DNA strand get DNA complement :\n";
+			cin >> dna;
+			cout << "The DNA complement is :" << get_dna_complement(dna) << endl;
+		}
+
+		if (reply == 3)
+		{
+
+			string exit_reply;
+
+			cout << "Are you sure you want to exit? Please type yes or no in lowercase\n";
+			cin >> exit_reply;
+			if (exit_reply == "yes")
+			{
+				cout << "Goodbye\n";
+				exit = 0;
+			}
+			
+			else if (exit_reply == "no")
+			{
+				cout << " Back to the menu\n";
+			}
+
+			else
+			{
+				cout <<"=================================\n";
+				cout << "Make sure you use lower case\n";
+				cout <<"=================================\n";
+			}
+		}
+
+	}
+	while (exit == 1);
+	
 	return 0;
 }
